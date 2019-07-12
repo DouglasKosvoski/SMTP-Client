b4e69132d64bf1d45985e6836db1dda7355d3694
Douglas Kosvoski <douglas.contactpro@gmail.com>
2019-07-12 10:17:31 -0300

    changed variable names

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import terminal
 import data
 
 if __name__ == '__main__':
-    t = terminal.Screen()
+    display = terminal.Screen()
     info = data.Data()
 
-    t.clear('''AUTOMATED EMAIL by DOUGLAS KOSVOSKI
+    display.clear('''AUTOMATED EMAIL by DOUGLAS KOSVOSKI
 GIT REPO https://github.com/DouglasKosvoski/Terminal_Email
-REMEMBER TO ALLOW THE APP https://myaccount.google.com/lesssecureapps
+REMEMBER TO ALLOW THE APP AT https://myaccount.google.com/lesssecureapps
 ''')
 
     info.get()
 
-t.clear()
+display.clear()

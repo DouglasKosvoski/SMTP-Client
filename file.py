8115b3899686c941053dba2c02b53beded92ffcb
Douglas Kosvoski <douglas.contactpro@gmail.com>
2019-07-12 10:18:02 -0300

    file to manage file attachs

---
+++ file.py
@@ -0,0 +1,19 @@
+import os
+from email import encoders
+from email.mime.base import MIMEBase
+
+class File():
+    def __init__(self):
+        pass
+
+    def annex(self):
+        file_path  = str(input("File PATH: "))
+        filename   = os.path.basename(file_path)
+        attachment = open(file_path, 'rb')
+
+        part = MIMEBase('application', 'octet-stream')
+        part.set_payload((attachment).read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition', 'attachment; filename= %s' % (filename))
+
+        return part

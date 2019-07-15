9a1fa30d48c5f386660f1ec64d0916c1c58240bf
Douglas Kosvoski <douglas.contactpro@gmail.com>
2019-07-15 12:59:51 -0300

    improved UI

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ if __name__ == '__main__':
     display = terminal.Screen()
     info = data.Data()
 
-    display.clear('''AUTOMATED EMAIL by DOUGLAS KOSVOSKI
-GIT REPO https://github.com/DouglasKosvoski/Terminal_Email
-REMEMBER TO ALLOW THE APP AT https://myaccount.google.com/lesssecureapps
+    display.clear('''
+*** REMEMBER TO ALLOW THE APP AT https://myaccount.google.com/lesssecureapps
+*** GIT REPO https://github.com/DouglasKosvoski/Terminal_Email
 ''')
 
     info.get()

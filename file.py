38cde5061b043b38c2eceb6c7cdcc9e33294d7ff
Douglas Kosvoski <douglas.contactpro@gmail.com>
2019-07-16 14:50:12 -0300

    check validation for files and directories based on PATH

--- file.py
+++ file.py
@@ -10,10 +10,17 @@ class File():
         pass
 
     def annex(self):
-        file_path  = str(input("File PATH: "))
+        file_path = str(input("File PATH: "))
+
+        while os.path.isfile(file_path) == False:
+            if os.path.isdir(file_path) == True:
+                print('Oops... You can not add a folder')
+            else:
+                print('Oops... File not found')
+            file_path = str(input("File PATH: "))
+
         filename   = os.path.basename(file_path)
         attachment = open(file_path, 'rb')
-
         part = MIMEBase('application', 'octet-stream')
         part.set_payload((attachment).read())
         encoders.encode_base64(part)
